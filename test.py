fad9715355dd560caded768e1ec2505ef36477aa
Mohit Raghav <79373327+rmohit011@users.noreply.github.com>
2021-09-01 15:15:27 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
-print("hello brother")
-print(20+2)
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+s=webdriver.Chrome(executable_path=r'F:\New folder\chromedriver.exe')
+s.get(r"http://demo.automationtesting.in/Windows.html")
+#//*[@id="Tabbed"]/a/button
+print(type(s))
+s.find_element_by_xpath("/html/body/div[1]/div/div/div/div[2]/div[1]/a").click()
+import time
+time.sleep(4)
+s.close()
+s.quit()
